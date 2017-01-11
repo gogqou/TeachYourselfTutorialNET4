462451b9cb324d1a3daf12c992529a84524ceb45
gou <gou@GOUDT>
2017-01-10 20:13:56 -0800

    Protocol form prototype start

---
+++ PrototypeMVCForm/Models/ProtocolFormModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrototypeMVCForm.Models
+{
+    public class ProtocolFormModel
+    {
+        public string ProtocolTitle { get; set; }
+        public int NumAnimals { get; set; }
+        public string ProtocolOwnerName { get; set; }
+        public string ProtocolBody { get; set; }
+    }
+}
